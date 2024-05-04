638ee469504b66f8798b259812442dc138410445
Alec Helmturner <alec@helmturner.dev>
2024-05-04 04:25:28 -0500

    chore: complete eslint/lint-staged setup

--- .eslintrc.js
+++ .eslintrc.js
@@ -19,7 +19,8 @@ export default {
     },
     "extends": [
         "plugin:@typescript-eslint/recommended",
-        "plugin:@typescript-eslint/recommended-requiring-type-checking"
+        "plugin:@typescript-eslint/recommended-requiring-type-checking",
+        "prettier"
     ],
     "parser": "@typescript-eslint/parser",
     "parserOptions": {
